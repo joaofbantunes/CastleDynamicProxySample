cd7a1f398ce1f94c7ac5a8f3cefc07847c3a1360
joaofbantunes <joaofbantunes@gmail.com>
2016-10-22 17:01:39 +0100

    Removing the Thread.Sleep from the timing benchmark because it didn't allow to have good visibility on the performance differences of the two approaches.

--- TimingSampleBenchmarksApplication/Service/StuffService.cs
+++ TimingSampleBenchmarksApplication/Service/StuffService.cs
@@ -7,7 +7,7 @@ namespace CodingMilitia.TimingSampleBenchmarksApplication.Service
     {
         public void DoTimeConsumingStuff()
         {
-            Thread.Sleep(1);
+            //Do slow stuff
         }
     }
 }
